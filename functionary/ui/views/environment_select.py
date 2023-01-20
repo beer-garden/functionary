e0cd5d961f57112bb910f8a473c71c1e7f66b3cc
Scott Taubman <86254668+scott-taubman@users.noreply.github.com>
2023-01-20 12:44:00 -0500

    #172 - Make user permissions available in the session data

--- functionary/ui/views/environment_select.py
+++ functionary/ui/views/environment_select.py
@@ -1,20 +1,20 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.exceptions import PermissionDenied
+from django.core.exceptions import ObjectDoesNotExist, PermissionDenied
 from django.http import HttpRequest
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
 from django.views.generic import TemplateView
 from django_htmx import http
 
-from core.models import Environment
 from ui.views.utils import set_session_environment
 
 
 class EnvironmentSelectView(LoginRequiredMixin, TemplateView):
     def post(self, request: HttpRequest):
         pk = request.POST["environment_id"]
-        environment = get_object_or_404(Environment, pk=pk)
 
-        if not request.user.environments.filter(pk=pk).exists():
+        try:
+            environment = request.user.environments.get(pk=pk)
+        except ObjectDoesNotExist:
             raise PermissionDenied
 
         set_session_environment(request, environment)
