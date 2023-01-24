a08b1c8df2c1d529cd6c014640549f6e98b25912
Scott Taubman <86254668+scott-taubman@users.noreply.github.com>
2023-01-24 10:14:39 -0500

    #178 - UI generic views

--- functionary/ui/views/environments/detail.py
+++ functionary/ui/views/environments/detail.py
@@ -9,15 +9,7 @@ from .utils import get_users
 
 class EnvironmentDetailView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
     model = Environment
-
-    def get_queryset(self):
-        return (
-            super()
-            .get_queryset()
-            .select_related(
-                "team",
-            )
-        )
+    queryset = Environment.objects.select_related("team").all()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -44,8 +36,3 @@ class EnvironmentDetailView(LoginRequiredMixin, UserPassesTestMixin, DetailView)
             else Variable.objects.none()
         )
         return context
-
-    def test_func(self):
-        return self.request.user.has_perm(
-            Permission.ENVIRONMENT_READ, self.get_object()
-        )
