d77a0a115b4a28e5e48f3f6ae8bc70c857c71975
Scott Taubman <86254668+scott-taubman@users.noreply.github.com>
2023-03-06 14:59:57 -0500

    #242 - Consolidate Task and WorkflowRun

--- functionary/core/models/task.py
+++ functionary/core/models/task.py
@@ -1,19 +1,23 @@
 """ Task model """
 import uuid
 from json import JSONDecodeError
-from typing import Optional, Union
+from typing import TYPE_CHECKING, Optional, Union
 
 from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from django.core.serializers.json import DjangoJSONEncoder
 from django.db import models
 
-from core.models import ModelSaveHookMixin, ScheduledTask
 from core.utils.parameter import validate_parameters
 
+if TYPE_CHECKING:
+    from core.models import Function, Workflow
 
-class Task(ModelSaveHookMixin, models.Model):
-    """A Task is an individual execution of a function
+
+class Task(models.Model):
+    """A Task is an individual execution of a function or workflow
 
     This model should always be queried with environment as one of the filter
     parameters. The indices are intentionally setup this way as all requests for task
@@ -21,10 +25,14 @@ class Task(ModelSaveHookMixin, models.Model):
 
     Attributes:
         id: unique identifier (UUID)
-        function: the function that this task is an execution of
+        tasked_type: the ContentType (model) of the object being tasked
+        tasked_id: the UUID of the object being tasked
+        tasked_object: foreign key to the object being tasked, built from tasked_type
+                        and tasked_id.
         environment: the environment that this task belongs to. All queryset filtering
                      should include an environment.
         parameters: JSON representing the parameters that will be passed to the function
+                    or workflow
         status: tasking status
         creator: the user that initiated the task
         created_at: task creation timestamp
@@ -44,7 +52,9 @@ class Task(ModelSaveHookMixin, models.Model):
     ]
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    function = models.ForeignKey(to="Function", on_delete=models.CASCADE)
+    tasked_type = models.ForeignKey(to=ContentType, on_delete=models.PROTECT)
+    tasked_id = models.UUIDField()
+    tasked_object = GenericForeignKey("tasked_type", "tasked_id")
     environment = models.ForeignKey(to="Environment", on_delete=models.CASCADE)
     parameters = models.JSONField(encoder=DjangoJSONEncoder)
     return_type = models.CharField(max_length=64, null=True)
@@ -53,11 +63,15 @@ class Task(ModelSaveHookMixin, models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     scheduled_task = models.ForeignKey(
-        ScheduledTask, null=True, blank=True, on_delete=models.SET_NULL
+        to="ScheduledTask", null=True, blank=True, on_delete=models.SET_NULL
     )
 
     class Meta:
         indexes = [
+            models.Index(
+                fields=["environment", "tasked_type", "tasked_id"],
+                name="task_contenttype",
+            ),
             models.Index(
                 fields=["environment", "status"], name="task_environment_status"
             ),
@@ -76,35 +90,53 @@ class Task(ModelSaveHookMixin, models.Model):
         return str(self.id)
 
     def _clean_environment(self):
-        """Ensures that the environment is correctly set to that of the function"""
+        """Ensures that the environment is correctly set to that of the function or
+        workflow
+        """
         if self.environment is None:
-            self.environment = self.function.package.environment
-        elif self.environment != self.function.package.environment:
+            self.environment = self.tasked_object.environment
+        elif self.environment != self.tasked_object.environment:
             raise ValidationError(
-                "Function does not belong to the provided environment"
+                "Function or Workflow being tasked does not belong to the provided "
+                "environment"
             )
 
     def _clean_parameters(self):
-        """Validate that the parameters conform to the function's schema"""
-        validate_parameters(self.parameters, self.function)
+        """Validate that the parameters conform to the schema of the function or
+        workflow
+        """
+        validate_parameters(self.parameters, self.tasked_object)
 
-    def _clean_function(self):
-        """Validate function is active for newly created tasks"""
-        if self._state.adding and self.function.active is False:
-            raise ValidationError("This function is not active")
+    def _clean_tasked_object(self):
+        """Validate tasked_object is active for newly created tasks"""
+        if self._state.adding and self.tasked_object.active is False:
+            raise ValidationError("This function or workflow is not active")
 
     def clean(self):
         """Model instance validation and attribute cleanup"""
         self._clean_environment()
         self._clean_parameters()
-        self._clean_function()
+        self._clean_tasked_object()
 
-    def post_create(self):
-        """Post create hooks"""
-        from core.utils.tasking import publish_task
+    def _update_status(self, status: str) -> None:
+        """Set the status if necessary and save"""
+        if self.status != status:
+            self.status = status
+            self.save()
 
-        publish_task.delay(self.id)
+    def complete(self) -> None:
+        """Set the status to COMPLETE"""
+        self._update_status(Task.COMPLETE)
 
+    def error(self) -> None:
+        """Set the status to ERROR"""
+        self._update_status(Task.ERROR)
+
+    def in_progress(self) -> None:
+        """Set the status to IN_PROGRESS"""
+        self._update_status(Task.IN_PROGRESS)
+
+    # TODO: Sort out what to do with this since it does not apply to workflows.
     @property
     def raw_result(self) -> Optional[str]:
         """Convenience property for accessing the result output"""
@@ -113,6 +145,7 @@ class Task(ModelSaveHookMixin, models.Model):
         except ObjectDoesNotExist:
             return None
 
+    # TODO: Sort out what to do with this since it does not apply to workflows.
     @property
     def result(self) -> Optional[Union[bool, dict, float, int, list, str]]:
         """Convenience property for accessing the result output loaded as JSON"""
@@ -123,6 +156,7 @@ class Task(ModelSaveHookMixin, models.Model):
         except JSONDecodeError:
             return self.taskresult.result
 
+    # TODO: Sort out what to do with this since it does not apply to workflows.
     @property
     def log(self) -> Optional[str]:
         """Convenience property for accessing the log output"""
@@ -133,5 +167,15 @@ class Task(ModelSaveHookMixin, models.Model):
 
     @property
     def variables(self):
-        """Returns the variables required by the function being tasked."""
-        return self.environment.variables.filter(name__in=self.function.variables)
+        """Returns the variables required by the function or workflow being tasked."""
+        return self.environment.variables.filter(name__in=self.tasked_object.variables)
+
+    @property
+    def function(self) -> "Function":
+        """Alias for tasked_object, mostly for type hinting purposes"""
+        return self.tasked_object  # type: ignore
+
+    @property
+    def workflow(self) -> "Workflow":
+        """Alias for tasked_object, mostly for type hinting purposes"""
+        return self.tasked_object  # type: ignore
