9cfccd619e10907cfaf3afb887b903f4d434e3e0
obr42 <107439850+obr42@users.noreply.github.com>
2023-04-05 19:56:53 +0000

    #310 - lint error

--- functionary/ui/tables/function.py
+++ functionary/ui/tables/function.py
@@ -23,7 +23,9 @@ class FunctionTable(tables.Table):
     )
 
     def render_name(self, value, record):
-        return format_html("{}: <span class='text-muted'>{}</span>", value, record.package)
+        return format_html(
+            "{}: <span class='text-muted'>{}</span>", value, record.package
+        )
 
     class Meta(BaseMeta):
         model = Function
