97f61be098fb99cd3625afc1671a748f2e038dbd
obr42 <107439850+obr42@users.noreply.github.com>
2023-04-18 16:37:44 -0400

    #337 - remove unused import

--- functionary/ui/urls.py
+++ functionary/ui/urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from django.views.generic import RedirectView
 
 from .views import (
     account,
